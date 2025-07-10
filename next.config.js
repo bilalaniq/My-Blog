3a2cd506d10277435f4d9faaf61ff41af6f60d74
bilalaniq <bilalaniq2@gmail.com>
2025-07-10 15:23:41 +0500

    fixed

--- next.config.js
+++ next.config.js
@@ -1,15 +1,11 @@
-const { withContentlayer } = require('next-contentlayer');
+/** @type {import('next').NextConfig} */
+
+const { withContentlayer } = require('next-contentlayer')
 
 const nextConfig = {
-  output: 'export',                   // for static export (out/ folder)
-  basePath: '/NullSect',             // important for GitHub Pages
-  assetPrefix: '/NullSect/',         // also important
-  images: {
-    unoptimized: true,               // needed for static export with next/image
-  },
-  compiler: {
-    removeConsole: true,
-  },
-};
+    compiler: {
+        removeConsole: true,
+    }
+}
 
-module.exports = withContentlayer(nextConfig);
+module.exports = withContentlayer({...nextConfig});
